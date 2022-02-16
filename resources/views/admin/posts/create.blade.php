a2bb32760137030a23f817b3c0cad1155f50fd2d
Francesco <scovascova@gmail.com>
2022-02-16 21:04:29 +0100

    value old

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -13,7 +13,7 @@
 
                             <div class="form-group">
                               <label for="title">Titolo</label>
-                              <input type="text" class="form-control @error('title') is invalid @enderror" id="title" name="title" placeholder="Inserisci titolo">
+                              <input type="text" class="form-control @error('title') is invalid @enderror" id="title" name="title" placeholder="Inserisci titolo" value="{{old('title')}}">
                               @error('title')
                                 <div class="alert alert-danger">{{$message}}</div>
                               @enderror
@@ -21,14 +21,14 @@
 
                             <div class="form-group">
                                 <label for="content">Contenuto</label>
-                                <textarea class="form-control @error('content') is invalid @enderror" id="content" name="content" placeholder="Inserisci contenuto"></textarea>
+                                <textarea class="form-control @error('content') is invalid @enderror" id="content" name="content" placeholder="Inserisci contenuto">{{old('content')}}</textarea>
                                 @error('content')
                                 <div class="alert alert-danger">{{$message}}</div>
                               @enderror
                             </div>
 
                             <div class="form-group form-check">
-                                <input type="checkbox" class="form-check-input @error('published') is invalid @enderror" id="published" name="published">
+                                <input type="checkbox" class="form-check-input @error('published') is invalid @enderror" id="published" name="published" {{old('content') ? 'checked' : ''}}>
                                 <label class="form-check-label" for="published">Pubblica</label>
                                 @error('published')
                                 <div class="alert alert-danger">{{$message}}</div>
