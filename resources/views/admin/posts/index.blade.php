e239d402818e8d28f0262c5fa55d9b4a993d3908
Francesco <scovascova@gmail.com>
2022-02-16 15:46:46 +0100

    tabella con pulsanti

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,16 +3,34 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">{{ __('Posts list') }}</div>
 
                     <div class="card-body">
-                        <ul>
-                            @foreach ($posts as $post)
-                                <li>{{$post->title}}</li>
-                            @endforeach
-                        </ul>
+                        
+                        <table class="table">
+                            <thead>
+                              <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Titolo</th>
+                                <th scope="col">Slug</th>
+                                <th scope="col">Azioni</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($posts as $post)
+                                    <tr>
+                                        <td>{{$post->id}}</td>
+                                        <td>{{$post->title}}</td>
+                                        <td>{{$post->slug}}</td>
+                                        <td>
+                                            <a href="{{route("posts.show", $post->id)}}"><button type="button" class="btn btn-primary">Visualizza</button></a>
+                                        </td>
+                                    </tr>
+                              @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
