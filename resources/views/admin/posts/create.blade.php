b38035991933df9428c42a85c302a5f8b05168e1
Francesco <scovascova@gmail.com>
2022-02-16 20:57:16 +0100

    alert aggiunti

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -13,17 +13,26 @@
 
                             <div class="form-group">
                               <label for="title">Titolo</label>
-                              <input type="text" class="form-control" id="title" name="title" placeholder="Inserisci titolo">
+                              <input type="text" class="form-control @error('title') is invalid @enderror" id="title" name="title" placeholder="Inserisci titolo">
+                              @error('title')
+                                <div class="alert alert-danger">{{$message}}</div>
+                              @enderror
                             </div>
 
                             <div class="form-group">
                                 <label for="content">Contenuto</label>
-                                <textarea class="form-control" id="content" name="content" placeholder="Inserisci contenuto"></textarea>
+                                <textarea class="form-control @error('content') is invalid @enderror" id="content" name="content" placeholder="Inserisci contenuto"></textarea>
+                                @error('content')
+                                <div class="alert alert-danger">{{$message}}</div>
+                              @enderror
                             </div>
 
                             <div class="form-group form-check">
-                                <input type="checkbox" class="form-check-input" id="published" name="published">
+                                <input type="checkbox" class="form-check-input @error('published') is invalid @enderror" id="published" name="published">
                                 <label class="form-check-label" for="published">Pubblica</label>
+                                @error('published')
+                                <div class="alert alert-danger">{{$message}}</div>
+                              @enderror
                             </div>
                             
                             <button type="submit" class="btn btn-primary">Crea</button>
